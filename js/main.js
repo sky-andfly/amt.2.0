f8023adc772b72f03bec6c87f0247ffa9fb2f2ef
Sergey Golubiev <skyandfly@outlook.com>
2020-02-17 02:01:15 +0300

    anchor

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let city = document.querySelector('.tab-city-header');
 let private = document.querySelector('.tab-private-header');
 let tabCity = document.querySelector('.tab-city');
 let tabPrivate = document.querySelector('.tab-private');
-
+let place = document.querySelector('.place');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup-close');
 // вешаем городу активность
 city.classList.add('tab-active');
 tabPrivate.classList.add('tab-hide');
@@ -20,3 +22,35 @@ city.addEventListener('click', function(){
     tabPrivate.classList.add('tab-hide');
     tabCity.classList.remove('tab-hide');
 })
+
+
+// кнопка наверх
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        if ($('#up-block').is(':hidden')) {
+            $('#up-block').css({opacity : 1}).fadeIn('slow');
+        }
+    } else { $('#up-block').stop(true, false).fadeOut('fast'); }
+});
+$('#up-block').click(function() {
+    $('html, body').stop().animate({scrollTop : 0}, 300);
+});
+
+// модальное окно карты
+place.addEventListener('click', function(){
+    popup.classList.add('popup-active');
+});
+popupClose.addEventListener('click', function(){
+    popup.classList.remove('popup-active');
+});
+
+// плавная прокрутка якорей
+ $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 300);
+    });
+});
+// убрать решетку из хеша
